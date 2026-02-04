625f31c3ae2605cd1c3963d0234bd2d4e0383135
Robertas Ankudovicius <r.ankudovicius@gmail.com>
2026-02-04 09:03:05 +0200

    Added data fetch

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import styles from "./styles.module.css";
 
 const Card = ({ img, title }) => {
+  const onClickCard = () => {
+    console.log(title);
+  };
+
   return (
-    <div className={styles.main}>
+    <div className={styles.main} onClick={onClickCard}>
       <img src={img} />
       <h3 className={styles.title}>{title}</h3>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,29 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Header from "../components/Header/Header";
 import Main from "../components/Main/Main";
 import Footer from "../components/Footer/Footer";
-import { useState } from "react";
 
 const Page = () => {
-  const [name, setName] = useState(0);
+  const [games, setGames] = useState();
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      "https://695dfc9f2556fd22f676eeca.mockapi.io/games",
+    );
+
+    console.log(response.data);
+    setGames(response.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
-      <button
-        onClick={() => {
-          setName(name + 1);
-        }}
-      >
-        Click
-      </button>
-      {name}
       <Header />
-      <Main />
+      <Main data={games} />
       <Footer />
     </>
   );
